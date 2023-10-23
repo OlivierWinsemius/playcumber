4915e24982b1d5a690564b26a262f27056d6c1c4
Olivier Winsemius <mail@olivierwinsemius.com>
2023-10-23 15:41:21 +0200

    chore: cleanup types

--- assertions/isPage.ts
+++ assertions/isPage.ts
@@ -1,10 +1,13 @@
 import CustomPage from "../pages/customPage";
 import CustomWorld from "../customWorld";
 
-export default function isPage<TargetPage extends typeof CustomPage>(
+export default function isPage<
+  TargetPage extends typeof CustomPage,
+  TargetWorld extends CustomWorld<InstanceType<TargetPage>>
+>(
   currentWorld: CustomWorld,
   targetPage: TargetPage
-): asserts currentWorld is CustomWorld<InstanceType<TargetPage>> {
+): asserts currentWorld is TargetWorld {
   if (!(currentWorld.page instanceof targetPage)) {
     throw new Error(`Page is not set to "${targetPage.name}"`);
   }
--- assertions/setPage.ts
+++ assertions/setPage.ts
@@ -1,10 +1,13 @@
 import CustomPage from "../pages/customPage";
 import CustomWorld from "../customWorld";
 
-export default function setPage<TargetPage extends typeof CustomPage>(
+export default function setPage<
+  TargetPage extends typeof CustomPage,
+  TargetWorld extends CustomWorld<InstanceType<TargetPage>>
+>(
   currentWorld: CustomWorld,
   targetPage: TargetPage
-): asserts currentWorld is CustomWorld<InstanceType<TargetPage>> {
+): asserts currentWorld is TargetWorld {
   const newPage = new targetPage(currentWorld.page.page);
   currentWorld.page = newPage;
 }
