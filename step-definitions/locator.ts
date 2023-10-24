136797b044b809afacdf12163cfac02093dec805
Olivier Winsemius <mail@olivierwinsemius.com>
2023-10-24 09:35:33 +0200

    feat: add role definition

--- step-definitions/locator.ts
+++ step-definitions/locator.ts
@@ -1,25 +1,18 @@
 import { expect } from "@playwright/test";
 import { Then, When } from "./steps";
 import pageHasLocator from "../assertions/pageHasLocator";
+import { Role } from "./parameters";
 
 When(
-  "I click {locator} button with text {string}",
-  async function (locator: string, text: string) {
+  "I click {section} {role} with text {string}",
+  async function (locator: string, role: Role, text: string) {
     pageHasLocator(this, locator);
-    await this.page.haha.getByRole("button", { name: text }).click();
-  }
-);
-
-When(
-  "I click {locator} link with text {string}",
-  async function (locator: string, text: string) {
-    pageHasLocator(this, locator);
-    await this.page[locator].getByRole("link", { name: text }).click();
+    await this.page[locator].getByRole(role, { name: text }).click();
   }
 );
 
 Then(
-  "I see {locator} has text {string}",
+  "I see {section} has text {string}",
   async function (locator: string, text: string) {
     pageHasLocator(this, locator);
     await expect(this.page[locator].getByText(text)).toBeVisible();
@@ -27,23 +20,11 @@ Then(
 );
 
 Then(
-  "I see {locator} has link with text {string}",
-  async function (locator: string, text: string) {
+  "I see {section} has {role} with text {string}",
+  async function (locator: string, role: Role, text: string) {
     pageHasLocator(this, locator);
-
     await expect(
-      this.page[locator].getByRole("link", { name: text })
-    ).toBeVisible();
-  }
-);
-
-Then(
-  "I see {locator} has button with text {string}",
-  async function (locator: string, text: string) {
-    pageHasLocator(this, locator);
-
-    await expect(
-      this.page[locator].getByRole("button", { name: text })
+      this.page[locator].getByRole(role, { name: text })
     ).toBeVisible();
   }
 );
--- step-definitions/parameters.ts
+++ step-definitions/parameters.ts
@@ -1,6 +1,7 @@
 import { defineParameterType } from "@cucumber/cucumber";
 import PlaywrightPage from "../pages/playwrightPage";
 import PlaywrightDocsPage from "../pages/playwrightDocsPage";
+import { Locator } from "@playwright/test";
 
 const pages = {
   home: PlaywrightPage,
@@ -16,7 +17,15 @@ defineParameterType({
 });
 
 defineParameterType({
-  name: "locator",
+  name: "section",
   regexp: /page|header|footer|sidebar|modal/,
+  transformer: (l) => l,
+});
+
+export type Role = Parameters<Locator["getByRole"]>[0];
+defineParameterType({
+  name: "role",
+  regexp:
+    /alert|alertdialog|application|article|banner|blockquote|button|caption|cell|checkbox|code|columnheader|combobox|complementary|contentinfo|definition|deletion|dialog|directory|document|emphasis|feed|figure|form|generic|grid|gridcell|group|heading|img|insertion|link|list|listbox|listitem|log|main|marquee|math|meter|menu|menubar|menuitem|menuitemcheckbox|menuitemradio|navigation|none|note|option|paragraph|presentation|progressbar|radio|radiogroup|region|row|rowgroup|rowheader|scrollbar|search|searchbox|separator|slider|spinbutton|status|strong|subscript|superscript|switch|tab|table|tablist|tabpanel|term|textbox|time|timer|toolbar|tooltip|tree|treegrid|treeitem/,
   transformer: (l) => l,
 });
